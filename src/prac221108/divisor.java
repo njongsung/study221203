5eb235fca816e224093594f93994475135298741
NJS <njongsung@gmail.com>
2022-11-09 00:00:35 +0900

    221107

---
+++ src/prac221108/divisor.java
@@ -0,0 +1,16 @@
+package prac221108;
+
+public class divisor {
+    public int solution(int n) {
+        int answer = 0;
+        int sum=0;
+
+
+        for(int i=1;i<=n;i++){
+            if(n%i==0) sum+=i;
+        }
+        answer=sum;
+
+        return answer;
+    }
+}
