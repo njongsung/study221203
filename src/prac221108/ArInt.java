f8121abc9eca77781bde056fd6aaa9c9ede04625
NJS <njongsung@gmail.com>
2022-11-08 23:47:43 +0900

    221107

---
+++ src/prac221108/ArInt.java
@@ -0,0 +1,16 @@
+package prac221108;
+
+public class ArInt {
+
+    public double solution(int[] arr) {
+        double answer =0;
+        int sum=0;
+        for (int i=0;i<arr.length;i++){
+           sum += arr[i];
+        }
+        answer=(double) sum/arr.length;
+
+        return answer;
+
+    }
+}
