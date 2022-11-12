1fd87f427dfb86daae63af2f00e8088ca6b6415c
NJS <njongsung@gmail.com>
2022-11-12 17:22:01 +0900

    221112

--- src/prac221112/InsertionSort.java
+++ src/prac221112/InsertionSort.java
@@ -1,6 +1,6 @@
 package prac221112;
 
-public class SelcetionSort {
+public class InsertionSort {
     public static void main(String[] args) {
         int n = 10;
         int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
