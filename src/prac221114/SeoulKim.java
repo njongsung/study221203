34440f863f1232e274359a0bdcc2449c4dd1002c
NJS <njongsung@gmail.com>
2022-11-14 22:25:07 +0900

    221114

---
+++ src/prac221114/SeoulKim.java
@@ -0,0 +1,19 @@
+package prac221114;
+
+public class SeoulKim {
+    public String solution(String[] seoul) {
+
+        String answer="";
+        String[] seoulfind = seoul;
+
+        String Kim="Kim";
+
+        for (int i=0; i< seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 "+i+"에 있다";
+                break;
+            }
+        }
+        return answer;
+    }
+}
