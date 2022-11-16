f2bec806ccb5923029528210b1b2aafd90767e14
NJS <njongsung@gmail.com>
2022-11-16 23:28:24 +0900

    221116

---
+++ src/prac221116/Inner.java
@@ -0,0 +1,14 @@
+package prac221116;
+
+public class Inner {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for(int i=0; i<a.length; i++){
+            answer += a[i]*b[i];
+        }
+
+        return answer;
+
+    }
+}
