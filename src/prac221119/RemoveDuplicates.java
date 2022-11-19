d12001d6a94c927aa58df7e908d220ddf5614bec
NJS <njongsung@gmail.com>
2022-11-20 00:29:13 +0900

    221119

--- src/prac221119/RemoveDuplicates.java
+++ src/prac221119/RemoveDuplicates.java
@@ -5,37 +5,30 @@ import java.util.ArrayList;
 
 
 public class RemoveDuplicates {
-    public static ArrayList remove(int[] arr) {
+    public static ArrayList solution(int[] arr) {
 
 
-            int n = arr.length;
-            int j = 0;
+        int n = arr.length;
+        int j = 0;
 
-            ArrayList<Integer> temp = new ArrayList<Integer>();
+        ArrayList<Integer> temp = new ArrayList<Integer>();
 
-            for (int i = 0; i < n - 1; i++) {
-                if (arr[i] != arr[i + 1]) {
-                        temp.add(arr[i]);
-                }
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temp.add(arr[i]);
             }
+        }
 
-            if(arr[n-1] == arr[n - 2]){
-                temp.add(arr[n-1]);
-            }
 
-            return temp;
+        if(temp.indexOf(temp.size() - 1) != arr[n-1]){
+            temp.add(arr[n-1]);
+        }
+
+
+        return temp;
 
     }
 
-
-
-    public static void main(String[] args) {
-        int[] a={1,1,3,3,0,1,1};
-        int n = a.length;
-
-        remove(a);
-
-
-
+    }
 
 }
