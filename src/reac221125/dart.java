e3e85b70b4ebffd78870640554309031a37ac67b
NJS <njongsung@gmail.com>
2022-11-25 08:37:08 +0900

    221125

--- src/reac221125/dart.java
+++ src/reac221125/dart.java
@@ -1,141 +1,120 @@
 package reac221125;
 
 public class dart {
-    public int solution(String dartResult) {
-        int answer = 0;
-//        int[] Pscore = {1,2,3,4,5,6,7,8,9,10};
-//        String[] bouns = {"S","T","D"};
-//        String[] option = {"*", "#"};
-
-        char[] process = dartResult.toCharArray();
-
-
-        return answer;
-
-    }
-
     private static int Solution(String dartResult) {
 
-        int answer=0;
-        int count=0;
-        int starcount=0;
-        int hashcount=0;
-        int allcount=0;
-        int pin=0;
-        int[] score= new int[3];
+        int answer = 0;
+        int count = 0;
+        int starcount = 0;
+        int hashcount = 0;
+        int allcount = 0;
+        int pin = 0;
+        int[] score = new int[3];
 
         char[] process = dartResult.toCharArray();
 
-        for (int i=0; i<process.length; i++){
-            switch(process[i]){     // 계산 전 점수
-                case 1:
-                    score[count]+=1;
-                    count++;
+        for (int i = 0; i < process.length; i++) {
+            switch (process[i]) {     // 계산 전 점수
+                case '1':
+                    score[count] += 1;
                     break;
-                case 2:
-                    score[count]+=2;
-                    count++;
+                case '2':
+                    score[count] += 2;
                     break;
-                case 3:
-                    score[count]+=3;
-                    count++;
+                case '3':
+                    score[count] += 3;
                     break;
-                case 4:
-                    score[count]+=4;
-                    count++;
+                case '4':
+                    score[count] += 4;
                     break;
-                case 5:
-                    score[count]+=5;
-                    count++;
+                case '5':
+                    score[count] += 5;
                     break;
-                case 6:
-                    score[count]+=6;
-                    count++;
+                case '6':
+                    score[count] += 6;
                     break;
-                case 7:
-                    score[count]+=7;
-                    count++;
+                case '7':
+                    score[count] += 7;
                     break;
-                case 8:
-                    score[count]+=8;
-                    count++;
+                case '8':
+                    score[count] += 8;
                     break;
-                case 9:
-                    score[count]+=9;
-                    count++;
+                case '9':
+                    score[count] += 9;
                     break;
-                case 10:
-                    score[count]+=10;
-                    count++;
+                default:
+                    score[count] += 10;
                     break;
             }
 
-            switch (process[i]){
+            switch (process[i]) {
                 case 'S':
-                    score[count-1]*=1;
+                    score[count] *= 1;
+                    count++;
                     break;
                 case 'D':
-                    Math.pow(score[count-1],2);
+                    Math.pow(score[count], 2);
+                    count++;
                     break;
                 case 'T':
-                    Math.pow(score[count-1],3);
+                    Math.pow(score[count], 3);
+                    count++;
                     break;
             }
 
-            if(hashcount==0) {
+            if (hashcount == 0) {
                 switch (process[i]) {
                     case '*':
                         if (starcount == 0 && allcount == 0) {
-                            score[count - 1] *= 2;
+                            score[count] *= 2;
                             starcount++;
                             allcount++;
                             break;
                         } else if (starcount > 0) {
-                            score[count - 1] *= 4;
+                            score[count] *= 4;
                             break;
                         }
 
                     case '#':
-                        score[count - 1] *= -1;
+                        score[count] *= -1;
                         hashcount++;
                         allcount++;
+                        pin = i - 2;
                         break;
                 }
-            }else{
+            } else {
                 switch (process[i]) {
                     case '*':
-                        if (starcount == 0 && allcount == 0) {
-                            score[count - 1] *= 2;
-                            starcount++;
-                            allcount++;
-                            break;
-                        } else if (starcount > 0 && allcount == 0) {
-                            score[count - 1] *= 4;
-                            break;
-                        }
+                        score[count]-=score[count];
+                        break;
+
 
                     case '#':
-                        score[count - 1] *= -1;
+                        score[count] *= -1;
                         hashcount++;
                         allcount++;
                         break;
+
+                }
 
             }
 
+
+            }
+        for (int j = 0; j < 3; j++) {
+            answer += score[j];
         }
 
-        for (int j=0; j<3; j++){
-            answer+=score[j];
-        }
+
         return answer;
+
+
     }
 
-
-
-
     public static void main(String[] args) {
-        String dartResult = "1S2D*3T";
-        Solution(dartResult);
+        String S = "1S2D*3T";
+        Solution(S);
+        System.out.println(Solution(S));
     }
 }
 
