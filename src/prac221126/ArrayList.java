aecb4c3fee23e98622e86ffc827190e1e9f9ec30
NJS <njongsung@gmail.com>
2022-11-27 19:22:05 +0900

    221127

--- src/prac221126/ArrayList.java
+++ src/prac221126/ArrayList.java
@@ -1,4 +1,38 @@
 package prac221126;
 
 public class ArrayList {
+    public static void main(String[] args) {
+        System.out.println(10 >> 1);
+
+        int[] arr = new int [5];
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = i;
+        }
+
+        int[] t = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            t[i] = arr[i] + 1;
+        }
+        // t의 이름이 arr로 바뀐 것.
+        arr = t;
+        for (int i = 0; i < 5; i++) {
+            arr[i+5] = i * 2;
+        }
+
+        for (var i: arr) {
+            System.out.print("" + i + " ");
+        }
+    }
+
+    // 성공 실패 여부 반환
+    // 추가는 맨 마지막 위치에 추가한다.(스택, 큐와 같음)
+//    public boolean add(int value){};
+
+    // 성공 실패 여부 반환( 에러 = 실패)
+    // 인덱스 위치의 데이터를 삭제 후, 뒤의 원소들을 하나씩 이동
+//    public boolean remove(int index){};
+
+    // 성공 값, 실패 -1
+//    public int indexOf(int index)
 }
--- src/prac221126/LinkedList.java
+++ src/prac221126/LinkedList.java
@@ -1,4 +1,100 @@
 package prac221126;
 
 public class LinkedList {
+    public class Node{
+        public Node next;
+        public int value;
+
+
+        public Node(int value, Node next) {
+            this.next = next;
+            this.value = value;
+        }
+    }
+    Node head;
+    int size;
+
+    public LinkedList(){
+        head = null;
+        size = 0;
+    }
+
+    public boolean insert(int index, int value){
+        if (size < index)
+            return false;
+        if (index == 0){
+            head = new Node(value, head);
+            size += 1;
+            return true;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++)
+            temp = temp.next;
+        Node next_after_node = temp.next;
+        temp.next = new Node(value, next_after_node);
+        size += 1;
+        return true;
+    }
+
+    public boolean remove(int index){
+        if (size <= index)
+            return false;
+        if (index - 1 < 0) {
+            head = head.next;
+            size -= 1;
+            return true;
+        }
+        Node temp = head;
+        for(int i = 0; i < index - 1; i++)
+            temp = temp.next;
+        temp.next = temp.next.next;
+        size -= 1;
+        return true;
+    }
+
+    public int search(int value){
+        Node cursor = head;
+        int idx = 0;
+        while(cursor != null){
+            if (cursor.value == value)
+                return idx;
+            cursor = cursor.next;
+            idx += 1;
+        }
+
+        return -1;
+    }
+
+    public void print(){
+        var cursor = head;
+        while(cursor != null){
+            System.out.print("" + cursor.value + ", ");
+            cursor = cursor.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        int[] test_data = {4,63,32,23,51,45,54};
+
+        for (var i : test_data) {
+            ll.insert(0, i);
+        }
+
+        ll.print();
+        ll.remove(3);
+        ll.print();
+
+        ll.remove(0);
+        ll.remove(0);
+        ll.print();
+
+        ll.insert(ll.size - 1, 99);
+        ll.print();
+
+        System.out.println(ll.search(32));
+        System.out.println(ll.search(100));
+
+    }
 }
